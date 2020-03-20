d2a35499a2afe914ffac0b08dcf8f6d7aa120c7b
Camilo Rosales <camilomarraui@gmail.com>
2020-03-20 17:23:00 -0500

    Add Bus class

---
+++ include/NES/bus.h
@@ -0,0 +1,10 @@
+#ifndef BUS_H
+#define BUS_H
+
+class Bus {
+  public:
+    Bus();
+    ~Bus();
+};
+
+#endif /* BUS_H */
---
+++ src/bus.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "NES/bus.h"
+
+Bus::Bus() {
+  std::cout << "Bus object initialized" << std::endl;
+}
+
+Bus::~Bus() { }
