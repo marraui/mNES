dd8f61a943cfe577a684da04d5b9ae17ae6d3ac7
Camilo Rosales <camilomarraui@gmail.com>
2020-03-20 17:50:56 -0500

    Add attributes to Cpu6502

--- include/NES/cpu6502.h
+++ include/NES/cpu6502.h
@@ -1,7 +1,16 @@
 #ifndef CPU6502_H
 #define CPU6502_H
 
+#include <cstdint>
+
 class Cpu6502 {
+  private:
+    u_int16_t programCounter;
+    u_int8_t accumulator;
+    u_int8_t xRegister;
+    u_int8_t yRegister;
+    u_int8_t stackPointer;
+    u_int8_t processorStatusRegister;
   public:
     Cpu6502();
     ~Cpu6502(); 
