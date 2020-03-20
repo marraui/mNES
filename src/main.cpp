3b4466b4ddb58abe43264751a5f37b9960e0482e
Camilo Rosales <camilomarraui@gmail.com>
2020-03-20 17:23:31 -0500

    Add Bus and Cpu6502 testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "NES/bus.h"
+#include "NES/cpu6502.h"
 
 int main() {
+  Bus bus = Bus();
+  Cpu6502 cpu6502 = Cpu6502();
   std::cout << "Hello World!" << std::endl;
   return 0;
 }
