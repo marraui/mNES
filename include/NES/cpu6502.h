854ef1a7f5d8af07c80d2eee13ccf91d650c5109
Camilo Rosales <camilomarraui@gmail.com>
2020-03-20 17:23:14 -0500

    Add Cpu6502 class

---
+++ include/NES/cpu6502.h
@@ -0,0 +1,10 @@
+#ifndef CPU6502_H
+#define CPU6502_H
+
+class Cpu6502 {
+  public:
+    Cpu6502();
+    ~Cpu6502(); 
+};
+
+#endif /* CPU6502_h */
---
+++ src/cpu6502.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "NES/cpu6502.h"
+
+Cpu6502::Cpu6502() {
+  std::cout << "CPU6502 initialized" << std::endl;
+}
+
+Cpu6502::~Cpu6502() { }
