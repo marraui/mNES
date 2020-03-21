e25a9237af2a3c343b2319cb14213c5435b2f10c
Camilo Rosales <camilomarraui@gmail.com>
2020-03-21 14:39:07 -0500

    Add bus initalization

--- include/NES/bus.h
+++ include/NES/bus.h
@@ -1,10 +1,17 @@
 #ifndef BUS_H
 #define BUS_H
 
+#include <cstdint>
+#include "cpu6502.h"
 class Bus {
+  private:
+    Cpu6502* cpu;
+    uint8_t ram[64 * 1064];
   public:
     Bus();
     ~Bus();
+    void write(uint16_t addr, uint8_t data);
+    uint8_t read(uint16_t addr);
 };
 
 #endif /* BUS_H */
--- src/bus.cpp
+++ src/bus.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <cstdint>
 #include "NES/bus.h"
-
+#include "NES/cpu6502.h"
+#include <functional>
 Bus::Bus() {
+  for (int i = 0; i < 64 * 1024; i++) this->ram[i] = 0x00;
+  Cpu6502* cpu = new Cpu6502([&](uint16_t addr, uint8_t data) -> void {
+    this->write(addr, data);
+  });
+  this->cpu = cpu;
   std::cout << "Bus object initialized" << std::endl;
 }
 
 Bus::~Bus() { }
+
+void Bus::write(uint16_t addr, uint8_t data) {
+  std::cout << "inside bus write" << std::endl;
+  if (addr < 0x0000 || addr > 0xFFFF) return;
+  this->ram[addr] = data;
+}
+
+uint8_t Bus::read(uint16_t addr) {
+  if (addr < 0x000 || addr > 0xFFFF) return 0x00;
+  return this->ram[addr];
+}
