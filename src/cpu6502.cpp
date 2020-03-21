caa77dbef56c773148d2a58ba474736120dcee4c
Camilo Rosales <camilomarraui@gmail.com>
2020-03-21 17:33:45 -0500

    Add read callback

--- src/cpu6502.cpp
+++ src/cpu6502.cpp
@@ -2,9 +2,16 @@
 #include "NES/cpu6502.h"
 #include <functional>
 
-Cpu6502::Cpu6502(std::function<void(uint16_t addr, uint8_t data)> write) {
+Cpu6502::Cpu6502(
+  std::function<void(uint16_t addr, uint8_t data)> write,
+  std::function<uint8_t(uint16_t addr)> read
+) {
   this->write = write;
+  this->read = read;
   std::cout << "CPU6502 callback initialization" << std::endl;
 }
 
 Cpu6502::~Cpu6502() { }
+
+void Cpu6502::clock() {
+}
