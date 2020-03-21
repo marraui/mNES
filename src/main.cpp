d654c3ccac07fb0d0d2472b49a9920147d4d02e4
Camilo Rosales <camilomarraui@gmail.com>
2020-03-21 14:39:40 -0500

    Remove unnecessary cpu instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include "NES/bus.h"
-#include "NES/cpu6502.h"
 
 int main() {
   Bus bus = Bus();
-  Cpu6502 cpu6502 = Cpu6502();
   std::cout << "Hello World!" << std::endl;
   return 0;
 }
