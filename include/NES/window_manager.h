4b1c55cce2c7f3b066cb48e57129ad21381e9c19
Camilo Rosales <camilomarraui@gmail.com>
2020-03-30 17:50:51 -0500

    Add WindowManager methods

--- include/NES/window_manager.h
+++ include/NES/window_manager.h
@@ -1,14 +1,31 @@
 #ifndef WINDOW_MANAGER_H
 #define WINDOW_MANAGER_H
 #include <SDL.h>
+#include "texture.h"
 
 class WindowManager {
   private:
+    SDL_Renderer* renderer;
     SDL_Window* window;
-    SDL_Surface* surface;
   public:
     WindowManager();
     ~WindowManager();
+    void init();
+    void destroy();
+    void clear();
+    void update();
+    void render(
+      Texture* texture,
+      int x,
+      int y,
+      SDL_Rect* clip = nullptr,
+      double angle = 0,
+      SDL_Point* center = nullptr,
+      SDL_RendererFlip flip = SDL_FLIP_NONE
+    );
+    SDL_Renderer* getRenderer();
+    int getWidth();
+    int getHeight();
 };
 
 #endif // WINDOW_MANAGER_H
--- src/window_manager.cpp
+++ src/window_manager.cpp
@@ -1,8 +1,102 @@
 #include "NES/window_manager.h"
+#include "NES/texture.h"
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 WindowManager::WindowManager() {
   this->window = nullptr;
-  this->surface = nullptr;
 }
 
 WindowManager::~WindowManager() {}
+
+void WindowManager::init() {
+  // Initialize SDL with video
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cout << "SDL couldn't initialize, SDL error:" << std::endl << SDL_GetError() << std::endl;
+    return;
+  }
+  if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+    std::cout << "Warning! Linear texture filtering not enabled" << std::endl;
+  }
+
+  // Create window
+  this->window = SDL_CreateWindow("NES", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+  if (this->window == nullptr) {
+    std::cout << "SDL couldn't create window, SDL error:" << std::endl << SDL_GetError() << std::endl;
+    return;
+  }
+  
+  this->renderer = SDL_CreateRenderer(this->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (this->renderer == nullptr) {
+    std::cout << "SDL couldn't create renderer, error: " << SDL_GetError() << std::endl;
+    return;
+  }
+
+  SDL_SetRenderDrawColor(this->renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+  if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+    std::cout <<"SDL_image couldn't initialize, error: " << SDL_GetError() << std::endl;
+    return;
+  }
+
+  if (TTF_Init() == -1) {
+    std::cout << "SDL_ttf couldn't initialize, error: " << SDL_GetError() << std::endl;
+    return;
+  }
+}
+
+void WindowManager::destroy() {
+  SDL_DestroyRenderer(this->renderer);
+  SDL_DestroyWindow(this->window);
+  this->window = nullptr;
+  this->renderer = nullptr;
+
+  //Quit SDL subsystems
+	TTF_Quit();
+	IMG_Quit();
+	SDL_Quit();
+}
+
+void WindowManager::render(
+  Texture* texture,
+  int x,
+  int y,
+  SDL_Rect* clip,
+  double angle,
+  SDL_Point* center,
+  SDL_RendererFlip flip
+) {
+  SDL_Rect renderQuad = { x, y, texture->getWidth() , texture->getHeight() };
+  if (clip != nullptr) {
+    renderQuad.w = clip->w;
+    renderQuad.h = clip->h;
+  }
+
+  SDL_RenderCopyEx(this->renderer, texture->getTexture(), clip, &renderQuad, angle, center, flip);
+}
+
+void WindowManager::clear() {
+  SDL_SetRenderDrawColor(this->renderer, 0x00, 0x00, 0x00, 0xFF);
+  SDL_RenderClear(this->renderer);
+}
+
+void WindowManager::update() {
+  SDL_RenderPresent(this->renderer);
+}
+
+SDL_Renderer* WindowManager::getRenderer() {
+  return this->renderer;
+}
+
+int WindowManager::getWidth() {
+  int w;
+  SDL_GetWindowSize(this->window, &w, nullptr);
+  return w;
+}
+
+int WindowManager::getHeight() {
+  int h;
+  SDL_GetWindowSize(this->window, nullptr, &h);
+  return h;
+}
