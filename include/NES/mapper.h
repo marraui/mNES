3378b75f1994cbc8401c9f7107d4fd1a628c48d6
Camilo Rosales <camilomarraui@gmail.com>
2020-05-17 14:31:49 -0500

    Fix initialize all methods to 0 so they can be overwritten

--- include/NES/mapper.h
+++ include/NES/mapper.h
@@ -6,10 +6,10 @@ class Mapper {
     uint8_t prgBanks;
     uint8_t chrBanks;
   public:
-    virtual uint16_t cpuMapRead(uint16_t addr);
-    virtual uint16_t cpuMapWrite(uint16_t addr, uint8_t data);
-    virtual uint16_t ppuMapRead(uint16_t addr);
-    virtual uint16_t ppuMapWrite(uint16_t addr, uint8_t data); 
+    virtual uint16_t cpuMapRead(uint16_t addr) = 0;
+    virtual uint16_t cpuMapWrite(uint16_t addr, uint8_t data) = 0;
+    virtual uint16_t ppuMapRead(uint16_t addr) = 0;
+    virtual uint16_t ppuMapWrite(uint16_t addr, uint8_t data) = 0;
 };
 
 #endif // MAPPER_H
